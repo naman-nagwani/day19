5a72713c8cacfa14d361e36e43539236464901b9
“naman-nagwani” <namannagwani@gmail.com>
2022-08-27 16:51:31 +0530

    UC5 added : Validate password one uppercase

--- src/com/bridgelabz/UserDetails.java
+++ src/com/bridgelabz/UserDetails.java
@@ -45,7 +45,7 @@ public class UserDetails {
     }
 
     private static boolean validatePassword(String password) {
-        Pattern pattern = Pattern.compile("^.{8,}$");
+        Pattern pattern = Pattern.compile("^(?=.*[A-Z]).{8,}$");
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
